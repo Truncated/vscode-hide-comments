4e822632c1db5f5cd470d4a4f1d084d270c42328
Truncated <djc664@gmail.com>
2024-05-02 17:11:51 -0400

    Update triggerRegexHide.ts

--- src/utils/triggerRegexHide.ts
+++ src/utils/triggerRegexHide.ts
@@ -1,11 +1,16 @@
 import { commands } from "vscode";
-import { CONFIG_REGEX, CONTEXT_KEYS } from "../extension";
+import { CONFIG_REGEX, CONFIG_COLOR, CONTEXT_KEYS } from "../extension";
 import { RegexModel } from "../models/RegexModel";
 import { ExtensionService } from "../services/ExtensionService";
 
 
-export const triggerRegexHide = async () => {
+export const triggerRegexHide = async (colors: any) => {
   const ext = ExtensionService.getInstance();
+  const colorConfig = ext.getSetting<boolean>(CONFIG_COLOR);
+  const enabled =
+    colors &&
+    (!colors["comments"] ||
+      (colors["comments"] && colors["comments"] !== colorConfig));
   const regExps = ext.getSetting<RegexModel[]>(CONFIG_REGEX);
   
   await commands.executeCommand('setContext', CONTEXT_KEYS.regex, (regExps && regExps.length > 0));
